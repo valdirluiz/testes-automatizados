71e98d48a6f1d10ccc5ae8ad4de0b1fd161fb642
Valdir Luiz <valdirluiz@localhost.localdomain>
2014-05-11 11:58:19 -0300

    Adicionando funcionalidade para encerar leilão

--- src/main/java/br/com/caelum/leilao/dominio/Leilao.java
+++ src/main/java/br/com/caelum/leilao/dominio/Leilao.java
@@ -1,41 +1,46 @@
 package br.com.caelum.leilao.dominio;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
 public class Leilao {
 
 	private String descricao;
+	private Calendar data;
 	private List<Lance> lances;
-
+	private boolean encerrado;
+	private int id;
+	
 	public Leilao(String descricao) {
+		this(descricao, Calendar.getInstance());
+	}
+	
+	public Leilao(String descricao, Calendar data) {
 		this.descricao = descricao;
+		this.data = data;
 		this.lances = new ArrayList<Lance>();
 	}
-
+	
 	public void propoe(Lance lance) {
-		if (lances.isEmpty() || podeDarLance(lance.getUsuario()))
+		if(lances.isEmpty() || podeDarLance(lance.getUsuario())) {
 			lances.add(lance);
+		}
 	}
 
 	private boolean podeDarLance(Usuario usuario) {
-		return (!ultimoLanceDado().getUsuario().equals(usuario) && quantidadeDeLancesDo(usuario) < 5);
+		return !ultimoLanceDado().getUsuario().equals(usuario) && qtdDeLancesDo(usuario) <5;
 	}
 
-	private int quantidadeDeLancesDo(Usuario usuario) {
+	private int qtdDeLancesDo(Usuario usuario) {
 		int total = 0;
-		for (Lance l : lances) {
-			if (l.getUsuario().equals(usuario))
-				total++;
+		for(Lance l : lances) {
+			if(l.getUsuario().equals(usuario)) total++;
 		}
 		return total;
 	}
-
-	private Lance ultimoLanceDado() {
-		return lances.get(lances.size() - 1);
-	}
-
+	
 	public void dobraLance(Usuario usuario) {
         Lance ultimoLance = ultimoLanceDo(usuario);
         if(ultimoLance!=null){
@@ -43,7 +48,7 @@ public class Leilao {
         }
         
     }
-
+	
 	private Lance ultimoLanceDo(Usuario usuario) {
 		Lance ultimo = null;
         for(Lance lance : lances) {
@@ -53,7 +58,11 @@ public class Leilao {
         }
 		return ultimo;
 	}
-	
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size()-1);
+	}
+
 	public String getDescricao() {
 		return descricao;
 	}
@@ -62,4 +71,23 @@ public class Leilao {
 		return Collections.unmodifiableList(lances);
 	}
 
+	public Calendar getData() {
+		return (Calendar) data.clone();
+	}
+
+	public void encerra() {
+		this.encerrado = true;
+	}
+	
+	public boolean isEncerrado() {
+		return encerrado;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
 }
